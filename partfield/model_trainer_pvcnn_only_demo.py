f0355baa767c8dcb1a578d7139884d48d9e5acfd
82magnolia <82magnolia@snu.ac.kr>
2025-05-16 13:34:17 +0900

    Fixed to default setup and added feature visualizations

--- partfield/model_trainer_pvcnn_only_demo.py
+++ partfield/model_trainer_pvcnn_only_demo.py
@@ -218,11 +218,15 @@ class Model(pl.LightningModule):
                 colors_255 = (data_reduced * 255).astype(np.uint8)
                 V = batch['vertices'][0].cpu().numpy()
                 F = batch['faces'][0].cpu().numpy()
+                if self.cfg.vertex_feature:
+                    colored_mesh = trimesh.Trimesh(vertices=V, faces=F, vertex_colors=colors_255, process=False)
+                else:
+                    colored_mesh = trimesh.Trimesh(vertices=V, faces=F, face_colors=colors_255, process=False)
 
-                # NOTE: Below assumes features to be extracted on vertices (vertex_feature = True)
-                colored_mesh = o3d.geometry.TriangleMesh(vertices=o3d.utility.Vector3dVector(V), triangles=o3d.utility.Vector3iVector(F))
-                colored_mesh.vertex_colors = o3d.utility.Vector3dVector(data_reduced)
-                o3d.io.write_triangle_mesh(f'{save_dir}/feat_pca_{uid}_{view_id}.ply', colored_mesh)
+                # When saving, use open3d to write meshes visualizable in Cloudcompare
+                colored_mesh_o3d = o3d.geometry.TriangleMesh(vertices=o3d.utility.Vector3dVector(V), triangles=o3d.utility.Vector3iVector(F))
+                colored_mesh_o3d.vertex_colors = o3d.utility.Vector3dVector(colored_mesh.visual.vertex_colors[:, :-1] / 255.)
+                o3d.io.write_triangle_mesh(f'{save_dir}/feat_pca_{uid}_{view_id}.ply', colored_mesh_o3d)
                 ############
                 torch.cuda.empty_cache()
 
@@ -275,10 +279,12 @@ class Model(pl.LightningModule):
                 data_reduced = (data_reduced - data_reduced.min()) / (data_reduced.max() - data_reduced.min())
                 colors_255 = (data_reduced * 255).astype(np.uint8)
 
-                # NOTE: Below assumes features to be extracted on vertices (vertex_feature = True)
-                colored_mesh = o3d.geometry.TriangleMesh(vertices=o3d.utility.Vector3dVector(V), triangles=o3d.utility.Vector3iVector(F))
-                colored_mesh.vertex_colors = o3d.utility.Vector3dVector(data_reduced)
-                o3d.io.write_triangle_mesh(f'{save_dir}/feat_pca_{uid}_{view_id}.ply', colored_mesh)
+                colored_mesh = trimesh.Trimesh(vertices=V, faces=F, face_colors=colors_255, process=False)
+
+                # When saving, use open3d to write meshes visualizable in Cloudcompare
+                colored_mesh_o3d = o3d.geometry.TriangleMesh(vertices=o3d.utility.Vector3dVector(V), triangles=o3d.utility.Vector3iVector(F))
+                colored_mesh_o3d.vertex_colors = o3d.utility.Vector3dVector(colored_mesh.visual.vertex_colors[:, :-1] / 255.)
+                o3d.io.write_triangle_mesh(f'{save_dir}/feat_pca_{uid}_{view_id}.ply', colored_mesh_o3d)
                 ############
 
         print("Time elapsed: " + str(time.time()-starttime))
